16a5e0c046f786aac2992d6089eddb1479b1d420
Zettrex <zettrex@hotmail.com>
2020-06-04 15:02:36 +0200

    mobile testing contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,6 +21,7 @@ export default function () {
 
     function _sendForm(data) {
         if (JSON.stringify(contact) !== JSON.stringify(data)) { //work around due to shallow compare
+            window.alert("we got in here")
             let registered = false;
             if (user.id) {
                 registered = true
@@ -42,6 +43,7 @@ export default function () {
                 headers: {"Content-Type": "application/x-www-form-urlencoded"},
                 body: `clientName=${encodeURIComponent(contact.clientName)}&clientRegistered=${encodeURIComponent(contact.clientRegistered)}&clientID=${contact.clientID}&clientEmail=${encodeURIComponent(contact.clientEmail)}&subject=${encodeURIComponent(contact.subject)}&message=${encodeURIComponent(contact.message)}`
             })
+                .then(() => window.alert("and we did fetch"))
                 .catch(error => window.alert(error));
         } else {
             setDuplicateMessage(true);
